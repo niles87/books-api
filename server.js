0134a8f56a55e13c5d1438098976da2119ea8c37
niles87 <nbing19@gmail.com>
2020-04-18 11:14:29 -0700

    mlab

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://niles:H#fJX2e9fseA6#Y@ds235947.mlab.com:35947/heroku_1cv8l6r4"
+);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
